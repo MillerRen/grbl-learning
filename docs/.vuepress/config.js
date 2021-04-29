b3aa860f7f9a5088b9d4b54610dfed87c8156d60
MillerRen <ren12345@126.com>
2021-04-29 10:48:32 +0800

    gh-pages

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Grbl 源码解析与移植',
+    description: 'Grbl源码精度与解析，代码行级中文注释并提供丰富详尽的案例展示。',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '指南', link: '/guide/' },
+            { text: 'Github', link: 'https://github.com/MillerRen/grbl-learning' },
+        ],
+        sidebar: [
+            ['/preface', '序章']
+        ]
+    }
+}
