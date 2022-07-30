51edf424802439d236998f0980b2831c942227d1
Miller Ren <ren12345@126.com>
2022-07-30 10:50:53 +0800

    logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,11 @@
 module.exports = {
     title: 'Grbl 源码解析与移植',
     description: 'Grbl源码精度与解析，代码行级中文注释并提供丰富详尽的案例展示。',
+    head: [
+        [
+            'link', { rel: 'icon', href: '/logo.png' }
+        ]
+    ],
     themeConfig: {
         nav: [
             { text: '首页', link: '/' },
